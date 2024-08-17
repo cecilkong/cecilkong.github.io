c888c7b78bcb23c84424683b1e9d3fc123022dd6
Cecil Kong <cecil.lh.kong@gmail.com>
2024-08-17 17:51:06 -0500

    Reformat navbar

--- my-app/src/routes/Header.svelte
+++ my-app/src/routes/Header.svelte
@@ -5,58 +5,74 @@
 </script>
 
 <header>
-	<div class="corner">
+	<!-- <div class="corner">
 		<a href="https://kit.svelte.dev">
 			<img src={logo} alt="SvelteKit" />
 		</a>
-	</div>
+	</div> -->
 
 	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
+		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/home' ? 'page' : undefined}>
-				<a href="/home">Cecil Kong</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/2D' ? 'page' : undefined}>
-				<a href="/2D">2D</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/3D' ? 'page' : undefined}>
-				<a href="/3D">3D</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/my-about' ? 'page' : undefined}>
-				<a href="/my-about">About</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
+		</svg> -->
+		<div class="title">
+			<ul>
+				<li aria-current={$page.url.pathname === '/home' ? 'page' : undefined}>
+					<a href="/home">Cecil Kong</a>
+				</li>
+			</ul>
+		</div>
+		<div class="page-links">
+			<ul>
+				<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+					<a href="/">Home</a>
+				</li>
+				<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+					<a href="/about">About</a>
+				</li>
+				<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+					<a href="/sverdle">Sverdle</a>
+				</li>
+				<li aria-current={$page.url.pathname === '/2D' ? 'page' : undefined}>
+					<a href="/2D">2D</a>
+				</li>
+				<li aria-current={$page.url.pathname === '/3D' ? 'page' : undefined}>
+					<a href="/3D">3D</a>
+				</li>
+				<li aria-current={$page.url.pathname === '/my-about' ? 'page' : undefined}>
+					<a href="/my-about">About</a>
+				</li>
+			</ul>
+		</div>
+		<!-- <svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
+		</svg> -->
 	</nav>
 
-	<div class="corner">
+	<!-- <div class="corner">
 		<a href="https://github.com/sveltejs/kit">
 			<img src={github} alt="GitHub" />
 		</a>
-	</div>
+	</div> -->
 </header>
 
 <style>
 	header {
 		display: flex;
-		justify-content: space-between;
+		/* justify-content: space-between; */
+		justify-content: center;
 	}
 
-	.corner {
+	.title {
+		align-self: flex-start;
+		border-style: solid;
+	}
+
+	.page-links {
+		align-self: flex-end;
+		border-style: solid;
+	}
+	/* .corner {
 		width: 3em;
 		height: 3em;
 	}
@@ -73,23 +89,28 @@
 		width: 2em;
 		height: 2em;
 		object-fit: contain;
-	}
+	} */
 
 	nav {
 		display: flex;
-		justify-content: center;
+		justify-content: space-between;
 		--background: rgba(255, 255, 255, 0.7);
 		font-family: var(--font-mono);
+		border-style: dotted;
+		flex: auto;
+		margin: 4% 12% 4% 12%;
 	}
 
-	svg {
+	/* svg {
 		width: 2em;
 		height: 3em;
 		display: block;
-	}
+		border-style: dotted;
+	} */
 
 	path {
 		fill: var(--background);
+		/* border-style: dotted; */
 	}
 
 	ul {
@@ -98,16 +119,19 @@
 		margin: 0;
 		height: 3em;
 		display: flex;
-		justify-content: center;
+		justify-content: space-between;
 		align-items: center;
 		list-style: none;
-		background: var(--background);
+		/* background: var(--background); */
 		background-size: contain;
+		border-style: dotted;
+		flex: auto;
 	}
 
 	li {
 		position: relative;
 		height: 100%;
+		/* border-style: dotted; */
 	}
 
 	li[aria-current='page']::before {
@@ -134,6 +158,7 @@
 		letter-spacing: 0.1em;
 		text-decoration: none;
 		transition: color 0.2s linear;
+		/* border-style: dotted; */
 	}
 
 	a:hover {
