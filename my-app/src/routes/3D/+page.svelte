47b2815d76c064e00b7da8ff3267f2c4000ff69c
Cecil Kong <cecil.lh.kong@gmail.com>
2024-08-24 18:46:30 -0500

    Set up basic 3D page

--- my-app/src/routes/3D/+page.svelte
+++ my-app/src/routes/3D/+page.svelte
@@ -4,63 +4,86 @@
 </svelte:head>
 
 <script lang>
-	import beret_dark from '$lib/images/2d/beret-girl-dark.png'
-    import flower_wall from '$lib/images/2d/flower-wall.jpg'
-    import georgia from '$lib/images/2d/georgia.png'
-    import girl_purple from '$lib/images/2d/girl-purple.png'
-    import hotel from '$lib/images/2d/hotel.png'
-    import living_room from '$lib/images/2d/living-room.png'
-    import poses from '$lib/images/2d/poses.png'
-    import restaurant from '$lib/images/2d/restaurant.png'
-    import sxr from '$lib/images/2d/sxr.png'
+	import idle from '$lib/images/3d/idle.gif'
+    import walk from '$lib/images/3d/walk.gif'
+    import run from '$lib/images/3d/run.gif'
+    import attack from '$lib/images/3d/attack.gif'
+    import special from '$lib/images/3d/special.gif'
+    import death from '$lib/images/3d/death.gif'
+    import damaged from '$lib/images/3d/damage.gif'
+    import jump from '$lib/images/3d/jump.gif'
+    import env_1 from '$lib/images/3d/env-1.png'
+    import env_2 from '$lib/images/3d/env-2.png'
+    import env_3 from '$lib/images/3d/env-3.png'
+    import env_video from '$lib/images/3d/duck.mp4'
+    import fireplace from '$lib/images/3d/final-portfo.mp4'
 </script>
 
-<p>
-    3D works
-</p>
+<p class="intro">3D works</p>
+<p>modeling, vfx, animation</p>
 
 <div class="gallery">
-    <div class="art-col">
-        <img src={restaurant} alt="Art" />
-        <img src={girl_purple} alt="Art" />
-        <img src={georgia} alt="Art" />
+    <div class="vfx">
+        <p>VFX</p>
+        <!-- <img src={fireplace} alt="Art" /> -->
+        <img src={special} alt="Art" />
+        <p>Animation, greenscreen, rotoscopes, color grading, particles, and final composite done with NukeX. 
+            Flame VFX made in Maya. Models used under CC. 2024</p>
     </div>
-    <div class="art-col">
-        <img src={beret_dark} alt="Art" />
-        <img src={flower_wall} alt="Art" />
-        <img src={hotel} alt="Art" />
+
+    <div class="modeling">
+        <p>Low Poly Environment</p>
+        <img src={special} alt="Art" />
+        <!-- <img src={env_video} alt="Art" /> -->
+        <img src={env_1} alt="Art" />
+        <img src={env_2} alt="Art" />
+        <img src={env_3} alt="Art" />
     </div>
-    <div class="art-col">
-        <img src={living_room} alt="Art" />
-        <img src={sxr} alt="Art" />
-        <img src={poses} alt="Art" />
+
+    <div class="animation">
+        <p>Character Moveset</p>
+        <img src={idle} alt="Art" />
+        <img src={walk} alt="Art" />
+        <img src={attack} alt="Art" />
+        <img src={special} alt="Art" />
+        <img src={jump} alt="Art" />
+        <img src={death} alt="Art" />
+        <img src={damaged} alt="Art" />
+        <img src={run} alt="Art" />
     </div>
 </div>
 
 <style>
-	img {
-        align-self: center;
-        width: 100%;
-        margin: 3%;
+    .intro {
+		margin: 10vh 0vw 0vh 0vw;
 	}
 
     .gallery {
-        /* margin: 15% 4% 15% 4%; */
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
 		justify-content: center;
-        /* flex: 0.2;
-		align-items: center; */
     }
 
-    .art-col {
-        /* margin: 15% 4% 15% 4%; */
+    .vfx {
+        border-style: dotted;
+        align-self: flex-start;
+    }
+
+    .modeling {
+        border-style: dotted;
+    }
+
+    .animation {
+        border-style: dotted;
+        align-self: flex-end;
+    }
+
+    /* .art-col {
         display: flex;
         flex-direction: column;
 		justify-content: top;
         width: 100%;
-        flex: 0.3;
+        flex: 0.4;
         margin: 1%;
-		/* align-items: center; */
-    }
+    } */
 </style>
