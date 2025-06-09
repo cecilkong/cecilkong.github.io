4fd88537d1e34415e90cfa6ffb941b123f362536
Cecil Kong <cecil.lh.kong@gmail.com>
2025-06-09 14:09:31 -0500

    Change about page visuals + pfp

--- my-app/src/routes/my-about/+page.svelte
+++ my-app/src/routes/my-about/+page.svelte
@@ -4,36 +4,28 @@
 </svelte:head>
 
 <script lang="ts">
-	import pfp_fallback from '$lib/images/about/pfp-fallback.jpg';
+	import pfp from '$lib/images/about/pfp.jpeg';
 </script>
 
 <div class="about">
     <div class="photo">
-        <img src={pfp_fallback} alt="About Photo" />
+        <img src={pfp} alt="About Photo" />
     </div>
     <div class="bio">
         <div class="text">
-            <p class="name">
-                <strong>Hi, I'm Cecil.</strong>
-            </p>
-            <p>
-                I’m a senior studying <strong>Computer Science</strong> with an emphasis in games 
+            <p class="name">Hi, I'm Cecil.</p>
+            <p class="name">I'm a gameplay engineer.</p>
+            <p class="desc">
+                I graduated with a B.S. in <strong>Computer Science</strong> with an emphasis in games
                 at the <strong>University of Southern California</strong>.
-                While I am primarily interested in gameplay programming, I've been making digital art
-                for several years—I often contribute a mix of both programming and art in many of my projects,
-                so you'll see some of both highlighted here. Ultimately, regardless of the medium,
-                I am dedicated to bringing creative ideas to life! In my free time, I enjoy longboard dancing,
-                journaling, and listening to music.
+            </p>
+            <p class="desc">
+                Along with gameplay programming, I've been making digital art
+                for several years—I often contribute both programming and art in many of my projects.
+                Ultimately, regardless of the medium, I am dedicated to bringing creative ideas to life.
+                In my free time, I enjoy longboard dancing, journaling, and listening to music.
             </p>
         </div>
-
-        <!-- <p class="role">
-            Current <br/>
-            Gov Affairs Intern @ Entertainment Software Association (ESA) <br/>
-            <br/>
-            Prev. <br/>
-            SWE Intern @ Ripple Effect Studios <br/> Gameplay Programming Intern @ Airstrafe Interactive
-        </p> -->
 
         <div class="links">
             <a href="https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=cecil.lh.kong@gmail.com" 
@@ -57,12 +49,19 @@
 <style>
     strong {
         font-weight: 900;
-        color:rgb(135, 156, 39);
-        font-size: 105%;
+        color:rgb(60, 91, 178);
+        font-size: 102%;
     }
 
+    :global(body.dark-mode) strong{
+        font-weight: 900;
+        color:rgb(166, 174, 198);
+        font-size: 102%;
+	}
+
 	img {
-        width: 25vw;
+        border-radius: 100%;
+        width: 35vw;
 	}
 
     .about {
@@ -84,8 +83,9 @@
         /* border-style: dotted; */
         margin: 0vh 0vw auto 0vw;
         align-self: flex-end;
-        margin-left: 5vw;
+        margin-left: 8vw;
         font-weight: 100;
+        text-align: end;
     }
 
     .text {
@@ -94,8 +94,18 @@
         margin-bottom: 0vh;
     }
 
+    .desc 
+    {
+        font-family: var(--font-body);
+        font-weight: 300;
+        font-size: 100%;
+    }
+
     .name {
         /* border-style: dotted; */
+        font-style: italic;
+        font-size: 125%;
+        font-weight: 800;
         margin-top: 0vh;
         margin-bottom: 0vh;
     }
@@ -104,29 +114,52 @@
         /* border-style: dotted; */
         display: flex;
         flex-direction: column;
-        width: 20vw;
+        width: 13vw;
+        justify-self: end;
     }
 
     a {
         border-style: solid;
-        height: 2.2vh;
-        background-color: rgb(135, 156, 39);
-        border-color: rgb(135, 156, 39);
-        color: #F1EDE7;
-        margin: 0.5vh 0vw 0.5vh 0vw;
-        padding: 1vh 1vw 1.5vh 1vw;
+        height: 2vh;
+        background-color: rgb(166, 174, 198);
+        border-color: rgb(166, 174, 198);
+        color:black;
+        /* border-color: black; */
+        /* margin: 1vh 0vw 1vh 0vw;
+        padding: 2vh 1vw 2vh 1vw; */
+        margin: 1vh 0vw;
+        padding: 1vh 1vw;
+        border-radius: 10%;
+        text-align: center;
     }
+
+    :global(body.dark-mode) a{
+        border-style: solid;
+        height: 2vh;
+        background-color: rgb(34, 49, 91);
+        border-color: rgb(34, 49, 91);
+        color:white;
+        margin: 1vh 0vw;
+        padding: 1vh 1vw;
+        border-radius: 10%;
+        text-align: center;
+	}
 
     a:hover {
         border-style: solid;
         background-color: #F1EDE7;
-        color: rgb(135, 156, 39);
+        color:rgb(166, 174, 198);
         box-shadow: 5px 5px 10px 0 rgb(138, 146, 181), 0 17px 50px 0 rgba(0, 0, 0, 0.196);
     }
 
-    /* .role 
-    {
-        font-size: 0.8em;
-        color:rgb(138, 146, 181);
-    } */
+    :global(body.dark-mode) a:hover{
+        border-style: solid;
+        height: 2vh;
+        background-color: rgb(0, 0, 0);
+        border-color: rgb(0, 0, 0);
+        margin: 1vh 0vw;
+        padding: 1vh 1vw;
+        border-radius: 10%;
+        text-align: center;
+	}
 </style>
